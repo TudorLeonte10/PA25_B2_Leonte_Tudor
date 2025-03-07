7658440cc1def0b535cd85140289b0dff1f20db3
TudorLeonte10 <153354694+TudorLeonte10@users.noreply.github.com>
2025-03-07 14:58:44 +0200

    Update Problem.java

--- Lab2/Compulsory/Homework2/src/Problem.java
+++ Lab2/Compulsory/Homework2/src/Problem.java
@@ -1,41 +1,50 @@
+public class Problem {/** Leonte Calin **/
+Student[] students;
+    Teacher[] teachers;
+    Project[] projects;
 
-public class Problem {
-    private Project[] projects;
-    private Student[] students;
-    private int projCount;
-    private int studCount;
-
-    public Problem(Project[] projects, Student[] students){
-        this.projects = projects;
+    public Problem (Student[] students, Teacher[] teachers, Project[] projects){
         this.students = students;
-    }
+        this.teachers = teachers;
+        this.projects = projects;
 
-    public void addStudent(Student student){
-        for(int i = 0; i < studCount; i++) {
-            if (students[i].equals(student)) {
-                System.out.println("Student" + student.getName() + "exists");
-                return;
+        for (int i = 0; i < students.length; i++) {
+            for (int j = i + 1; j < students.length; j++) {
+                if (students[i].equals(students[j])) {
+                    throw new IllegalArgumentException("Există deja un student cu registrationNumber " + students[i].getRegNumber());
+                }
             }
         }
-        students[studCount++] = student;
-    }
 
-    public void addProject(Project project){
-        for(int i = 0; i < projCount; i++) {
-            if (projects[i].equals(project)) {
-                System.out.println("Project" + project.getName() + "exists");
-                return;
+        for (int i = 0; i < teachers.length; i++) {
+            for (int j = i + 1; j < teachers.length; j++) {
+                if (teachers[i].equals(teachers[j])) {
+                    throw new IllegalArgumentException("Există deja un profesor cu aceleași date.");
+                }
             }
         }
-        projects[projCount++] = project;
-    }
 
-    public Person[] getAllPersonsInvolved(){
-        Person[] persons = new Person[studCount + projCount];
-        for (int i = 0; i < studCount; i++) {
-            persons[i] = students[i];
+        for (int i = 0; i < projects.length; i++) {
+            for (int j = i + 1; j < projects.length; j++) {
+                if (projects[i].equals(projects[j])) {
+                    throw new IllegalArgumentException("Există deja un proiect cu acelasi titlu si tip.");
+                }
+            }
         }
-        return persons;
+
     }
 
+    public Person[] getAllPersons() {
+        Person[] allPersons = new Person[students.length + teachers.length];
+        int contor = 0;
+        for (Student student : students) {
+            allPersons[contor] = student;
+            contor++;
+        }
+        for (Teacher teacher : teachers) {
+            allPersons[contor] = teacher;
+            contor++;
+        }
+        return allPersons;
+    }
 }
