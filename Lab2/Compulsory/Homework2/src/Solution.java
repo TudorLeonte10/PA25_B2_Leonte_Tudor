8e76a986eda5ab1f6985a30a87e690f276f1d016
TudorLeonte10 <153354694+TudorLeonte10@users.noreply.github.com>
2025-03-07 14:59:41 +0200

    Update Solution.java

--- Lab2/Compulsory/Homework2/src/Solution.java
+++ Lab2/Compulsory/Homework2/src/Solution.java
@@ -1,3 +1,24 @@
+public class Solution { 
+    public static void allocateProjects(Problem problem) {
+        boolean[] allocatedProjects = new boolean[problem.projects.length];
+        String[] allocations = new String[problem.students.length];
 
-public class Solution {
+        for (int i = 0; i < problem.students.length; i++) {
+            for (Project preferredProject : problem.students[i].availableProjects) {
+                for (int j = 0; j < problem.projects.length; j++) {
+                    if (problem.projects[j].equals(preferredProject) && !allocatedProjects[j]) {
+                        allocations[i] = problem.students[i].name + " va face proiectul " + problem.projects[j].title;
+                        allocatedProjects[j] = true;
+                        break;
+                    }
+                }
+                if (allocations[i] != null) break;
+            }
+        }
+
+        for (String allocation : allocations) {
+            if (allocation != null)
+                System.out.println(allocation);
+        }
+    }
 }
