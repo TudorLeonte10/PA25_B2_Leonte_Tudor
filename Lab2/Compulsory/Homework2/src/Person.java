2ca00db836b7fd2e036f808354a1196534344fc5
TudorLeonte10 <153354694+TudorLeonte10@users.noreply.github.com>
2025-03-07 14:58:20 +0200

    Update Person.java

--- Lab2/Compulsory/Homework2/src/Person.java
+++ Lab2/Compulsory/Homework2/src/Person.java
@@ -1,43 +1,22 @@
 import java.util.Date;
 import java.util.Objects;
 
-public class Person {
-    protected String name;
-    protected String dateOfBirth;
+public class Person { /** Leonte Tudor**/
+protected String name;
+    protected String birthDate;
 
-    public Person(String name, String dateOfBirth){
+    public Person (String name, String birthDate){
         this.name = name;
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
+        this.birthDate = birthDate;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return Objects.equals(name, person.name) && Objects.equals(dateOfBirth, person.dateOfBirth);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", dateOfBirth=" + dateOfBirth +
-                '}';
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (getClass() != obj.getClass())
+            return false;
+        Person person = (Person) obj;
+        return Objects.equals(name, person.name) && Objects.equals(birthDate, person.birthDate);
     }
 }
