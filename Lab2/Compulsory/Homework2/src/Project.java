500271d66e789ecf1f354d0feaf31e3d563831c4
TudorLeonte10 <153354694+TudorLeonte10@users.noreply.github.com>
2025-03-07 14:59:15 +0200

    Update Project.java

--- Lab2/Compulsory/Homework2/src/Project.java
+++ Lab2/Compulsory/Homework2/src/Project.java
@@ -1,35 +1,21 @@
+import java.util.Objects;
 
-
-public class Project {
-    private String name;
+public class Project { 
+String title;
     private Type type;
 
-    public Project(String name, Type type){
-        this.name = name;
+    public Project (String title, Type type)
+    {
+        this.title = title;
         this.type = type;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
     }
 
     @Override
-    public String toString() {
-        return "Project{" +
-                "name='" + name + '\'' +
-                ", type=" + type +
-                '}';
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Project)) return false;
+        Project project = (Project) obj;
+        return Objects.equals(project.title, title) && Objects.equals(project.type, type);
     }
 }
