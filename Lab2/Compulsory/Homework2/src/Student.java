af72b8255a60a736346e2b1a2932b0a03e4fddad
TudorLeonte10 <153354694+TudorLeonte10@users.noreply.github.com>
2025-03-08 19:27:59 +0200

    Update Student.java

--- Lab2/Compulsory/Homework2/src/Student.java
+++ Lab2/Compulsory/Homework2/src/Student.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.Objects;
 
-public class Student extends Person{ /** Leonte Calin **/
+public class Student extends Person{ 
 Project[] availableProjects;
     private int regNumber;
 
