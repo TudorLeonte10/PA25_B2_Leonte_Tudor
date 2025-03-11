e959404cac9093497820ad89c227f1e92673ffd1
TudorLeonte10 <153354694+TudorLeonte10@users.noreply.github.com>
2025-03-11 11:37:11 +0200

    Update Problem.java

--- Lab2/Compulsory/Homework2/src/Problem.java
+++ Lab2/Compulsory/Homework2/src/Problem.java
@@ -11,7 +11,7 @@ Student[] students;
         for (int i = 0; i < students.length; i++) {
             for (int j = i + 1; j < students.length; j++) {
                 if (students[i].equals(students[j])) {
-                    throw new IllegalArgumentException("Există deja un student cu registrationNumber " + students[i].getRegNumber());
+                    throw new IllegalArgumentException("Exista deja un student cu registrationNumber " + students[i].getRegNumber());
                 }
             }
         }
@@ -19,7 +19,7 @@ Student[] students;
         for (int i = 0; i < teachers.length; i++) {
             for (int j = i + 1; j < teachers.length; j++) {
                 if (teachers[i].equals(teachers[j])) {
-                    throw new IllegalArgumentException("Există deja un profesor cu aceleași date.");
+                    throw new IllegalArgumentException("Exista deja un profesor cu aceleași date.");
                 }
             }
         }
@@ -27,7 +27,7 @@ Student[] students;
         for (int i = 0; i < projects.length; i++) {
             for (int j = i + 1; j < projects.length; j++) {
                 if (projects[i].equals(projects[j])) {
-                    throw new IllegalArgumentException("Există deja un proiect cu acelasi titlu si tip.");
+                    throw new IllegalArgumentException("Exista deja un proiect cu acelasi titlu si tip.");
                 }
             }
         }
