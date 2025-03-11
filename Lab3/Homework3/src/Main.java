b21a0e122923a4f9d29b8a55f78afbe2859dfc66
TudorLeonte10 <153354694+TudorLeonte10@users.noreply.github.com>
2025-03-11 16:34:31 +0200

    Update Main.java

--- Lab3/Homework3/src/Main.java
+++ Lab3/Homework3/src/Main.java
@@ -1,15 +1,27 @@
+import java.time.LocalTime;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Airport airport = new Airport("IasiAirport", 3 );
+
+        Flight flight1 = new Flight("A123", LocalTime.of(10, 0), LocalTime.of(10, 30));
+        Flight flight2 = new Flight("B456", LocalTime.of(10, 15), LocalTime.of(10, 45));
+        Flight flight3 = new Flight("C789", LocalTime.of(11, 0), LocalTime.of(11, 30));
+        Flight flight4 = new Flight("D101", LocalTime.of(10, 40), LocalTime.of(11, 10));
+        Flight flight5 = new Flight("E202", LocalTime.of(11, 20), LocalTime.of(11, 50));
+
+        Schedule problem = new Schedule(airport);
+
+        problem.addFlight(flight1);
+        problem.addFlight(flight2);
+        problem.addFlight(flight3);
+        problem.addFlight(flight4);
+        problem.addFlight(flight5);
+
+        problem.solve();
+        problem.printSol();
     }
 }
